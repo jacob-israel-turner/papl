7663525239c50215b9e52b5931a9aad9ec38031e
Jacob Turner <j.israel.turner@gmail.com>
2017-10-05 20:10:43 -0600

    Basic frontend

---
+++ src/App.js
@@ -0,0 +1,84 @@
+import React, { Component } from 'react';
+import AceEditor from 'react-ace';
+import autoBind from 'react-autobind';
+
+import 'brace/mode/javascript';
+import 'brace/snippets/javascript';
+import 'brace/theme/monokai';
+import 'brace/ext/language_tools';
+
+class App extends Component {
+  constructor() {
+    super();
+    autoBind(this);
+    this.state = {
+      code: ''
+    }
+  }
+
+  onChange(e) {
+    this.setState({ code: e });
+  }
+
+  run() {
+    console.log(this.state.code);
+  }
+
+  render() {
+    // return <AceEditor
+    //   mode="javascript"
+    //   theme="monokai"
+    //   onChange={this.onChange}
+    //   name="UNIQUE_ID_OF_DIV"
+    //   editorProps={{$blockScrolling: true}}
+    //   setOptions={{
+    //     enableBasicAutocompletion: false,
+    //     enableLiveAutocompletion: true,
+    //     showLineNumbers: true,
+    //     enableSnippets: false,
+    //     tabSize: 2,
+    //   }}
+    // />
+    return (
+      <div style={{
+        
+      }}>
+        <AceEditor
+          style={{
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            right: 0,
+            bottom: 0
+          }}
+          height='100vh'
+          width='100vw'
+          mode="javascript"
+          theme="monokai"
+          name="blah2"
+          onChange={this.onChange}
+          fontSize={14}
+          showPrintMargin={true}
+          showGutter={true}
+          highlightActiveLine={true}
+          value={this.state.code}
+          setOptions={{
+            enableBasicAutocompletion: false,
+            enableLiveAutocompletion: true,
+            enableSnippets: false,
+            showLineNumbers: true,
+            tabSize: 2,
+          }}
+        />
+        <button style={{
+          position: 'absolute',
+          top: 10,
+          right: 10,
+          zIndex: 100
+        }} onClick={this.run}>RUN</button>
+      </div>
+    )
+  }
+}
+
+export default App;
