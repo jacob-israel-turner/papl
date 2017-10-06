c271da6f02765dd53418c6212a9ece4fa8b6030a
Jacob Turner <j.israel.turner@gmail.com>
2017-10-05 21:59:01 -0600

    Evaluate working
    This is going to need to change.
    I'll need to install modules based on require statements.
    That'll be hard to do while passing the javascript directly to spawn.

--- server/index.js
+++ server/index.js
@@ -1,7 +1,9 @@
+import 'babel-polyfill';
 import Koa from 'koa';
 import Router from 'koa-router';
 import cors from 'koa-cors';
 import bodyParser from 'koa-bodyparser';
+import { spawn } from 'child_process';
 
 const app = new Koa();
 const router = new Router();
@@ -9,15 +11,15 @@ const router = new Router();
 const port = 9001;
 
 router
-  .post('/eval', function (cxt, next) {
+  .post('/eval', async function (cxt, next) {
     try {
-      const result = eval(cxt.request.body.code);
+      const result = await evalCode(cxt.request.body.code);
       cxt.body = result;
     } catch (e) {
+      console.log(e);
       cxt.status = 400;
       cxt.body = e;
     }
-    cxt.body = 'doneee';
   })
 
 app
@@ -27,4 +29,22 @@ app
   .use(router.allowedMethods())
   .listen(port);
 
-
+function evalCode(code) {
+  return new Promise((resolve, reject) => {
+    const evaluate = spawn('node', ['-e', code]);
+    let all = [];
+    let out = [];
+    let err = [];
+    evaluate.stdout.on('data', (data) => {
+      all.push(data.toString());
+      out.push(data.toString());
+    });
+    evaluate.stderr.on('data', (data) => {
+      all.push(data.toString());
+      err.push(data.toString());
+    });
+    evaluate.on('close', (code) => {
+      resolve({ all, out, err, code });
+    });
+  })
+}
